47e86d3ae5c498169a015e059f4d9b66bbb1cc8f
0xfinder <0xfinder@protonmail.com>
2022-06-10 18:04:40 +0800

    arch

--- .config/nvim/lua/plugins/init.lua
+++
@@ -1,6 +0,0 @@
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'andweeb/presence.nvim'
-  use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
-end)
